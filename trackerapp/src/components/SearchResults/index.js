616c1c973217dfc1fe868b3960430ed88c931b94
Gavin Phill <carpe.gavin@gmail.com>
2021-03-11 20:42:53 -0600

    add sorting method. No errors but it's not showing the data in the DOM now

--- trackerapp/src/components/SearchResults/index.js
+++ trackerapp/src/components/SearchResults/index.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./style.css";
 
+
+
 function SearchResults(props) {
   const { results } = props;
   return (
   
-  <table class="table table-striped table-bordered" id="sortTable">
+  
+  <table class="table table-striped table-bordered" id="empTable">
     <thead>
+    
       <tr>
+        
         <th>Image</th>
-        <th>Name</th>
+        <th onclick="sortTable(0)">Name</th>
         <th>Email</th>
         <th>Cell</th>
       </tr>
@@ -18,18 +23,54 @@ function SearchResults(props) {
     <tbody>
       {results.map(result => (
         <tr key={result}>
-          <td>{result.picture.medium}</td>
+          <td><img src={result.picture.medium} alt={result.name.first}></img></td>
           <td>{result.name.first} {result.name.last}</td>
           <td>{result.email}</td>
           <td>{result.cell}</td>
         </tr>
     ))}
     </tbody>
-  </table>
-  );
+  </table>,
+
+  function sortTable(n) {
+      var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+      table = document.getElementById("empTable");
+      switching = true;
+      dir = "asc";
+      while (switching) {
+        switching = false;
+        rows = table.rows;
+        for (i = 1; i < (rows.length - 1); i++) {
+          shouldSwitch = false;
+          x = rows[i].getElementsByTagName("TD")[n];
+          y = rows[i + 1].getElementsByTagName("TD")[n];
+            if (dir === "asc") {
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+              shouldSwitch = true;
+              break;
+            }
+          } else if (dir === "desc") {
+            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+              shouldSwitch = true;
+              break;
+            }
+          }
+        }
+        if (shouldSwitch) {
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+          switching = true;
+          switchcount++;
+        } else {
+          if (switchcount === 0 && dir === "asc") {
+            dir = "desc";
+            switching = true;
+          }
+        }
+      }
+    }
+  
+);
 }
-
-
 
 export default SearchResults;
 

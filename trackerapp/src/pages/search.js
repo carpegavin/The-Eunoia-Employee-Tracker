57766af8ec531f1171f267b70a1148076296e1bd
Gavin Phill <carpe.gavin@gmail.com>
2021-03-12 19:21:15 -0600

    remove employeeCard component. remove emplyees json

--- trackerapp/src/pages/search.js
+++ trackerapp/src/pages/search.js
@@ -3,60 +3,61 @@ import API from "../utils/API";
 import Container from "../components/Container";
 import SearchForm from "../components/SearchForm";
 import SearchResults from "../components/SearchResults";
-import Header from "../components/Header"
+import Header from "../components/Header";
 
-<Header />
-function Search () {
+<Header />;
+function Search() {
   const [searchState, setSearchState] = useState({
     search: "",
     employees: [],
     results: [],
-    error: ""
+    error: "",
   });
-  const [employee, setEmployee] = useState([])
-  const [results, setResults] = useState([])
- //hook above!______________________________
-  useEffect(() =>{
+  const [employee, setEmployee] = useState([]);
+  const [results, setResults] = useState([]);
+  //hook above!______________________________
+  useEffect(() => {
     API.getRandomEmployee()
-      .then(res => setEmployee( res.data.results))
-      .then(res => setResults(employee))
-      .catch(err => console.log(err))
-  }, []) 
+      .then((res) => setEmployee(res.data.results))
+      .then((res) => setResults(employee))
+      .catch((err) => console.log(err));
+  }, []);
 
-  const handleInputChange = event => {
+  const handleInputChange = (event) => {
     let searchTerm = event.target.value;
 
-    let filtered = employee.filter(query => query.name.first.includes(searchTerm))
+    let filtered = employee.filter((query) =>
+      query.name.first.includes(searchTerm)
+    );
 
     setResults(filtered);
-    console.log(employee)
+    console.log(employee);
   };
 
-const handleFormSubmit = event => {
+  const handleFormSubmit = (event) => {
     event.preventDefault();
     API.getEmployees(searchState.employees)
-      .then(res => {
+      .then((res) => {
         if (res.data.status === "error") {
           throw new Error(res.data.message);
         }
         this.setState({ results: res.data.message, error: "" });
       })
-      .catch(err => this.setState({ error: err.message }));
+      .catch((err) => this.setState({ error: err.message }));
   };
-  
-    return (
-      <div>
-        <Container style={{ minHeight: "80%" }}>
-          <SearchForm
-            handleFormSubmit={handleFormSubmit}
-            handleInputChange={handleInputChange}
-            employees={searchState.employees}
-          />
-          <SearchResults  results={results} />
-        </Container>
-      </div>
-    );
-  }
 
-  
+  return (
+    <div>
+      <Container style={{ minHeight: "80%" }}>
+        <SearchForm
+          handleFormSubmit={handleFormSubmit}
+          handleInputChange={handleInputChange}
+          // employees={searchState.employees}
+        />
+        <SearchResults results={results} />
+      </Container>
+    </div>
+  );
+}
+
 export default Search;

0024c7f52b2222648f431fb9720577fcfb6dd52c
Gavin Phill <carpe.gavin@gmail.com>
2021-03-12 19:09:48 -0600

    removed alert component. removed import from employees json from app.js

--- trackerapp/src/App.js
+++ trackerapp/src/App.js
@@ -1,42 +1,47 @@
- /* eslint-disable no-undef */
- import employees from "./employees.json";
+/* eslint-disable no-undef */
+// import employees from "./employees.json";
 import React, { Component } from "react";
-// import EmployeeCard from "./components/EmployeeCard";
 import Wrapper from "./components/Container";
 import Search from "./pages/search";
 import Header from "./components/Header";
 // import { BrowserRouter as Router, Route } from "react-router-dom";
-// import Header from "./components/Header";
-// import React, { useState, useEffect } from "react";
 
 class App extends Component {
-  
-  state = {
-    employees: employees.results
-  };
+  // state = {
+  //   employees: employees.results,
+  // };
 
   render() {
     return (
-      
       <Wrapper>
-          <Header />
-          <Search />
+        <Header />
+        <Search />
       </Wrapper>
     );
   }
 }
 export default App;
 
+
+
+//-----------Saving code below as reference for future projects-------------------
+
+
+//-------------Hardcoded from employee json-------------------------------
+
 // {this.state.employees.map(employee => (
-          
+
 //   <EmployeeCard
 //     name={`${employee.name.first} ${employee.name.last}`}
 //     phone={employee.phone}
 //     email={employee.email}
 //     picture={employee.picture.medium}
 //   />
-  
+
 // ))}
+
+
+//--------------------------Multiple Pages---------------------------
 
 // function App() {
 //   return (

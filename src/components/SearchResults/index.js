a229cb67e313fad71a9f9fff891271d08e2418b4
Gavin Phill <carpe.gavin@gmail.com>
2021-03-13 20:56:48 -0600

    add sort to search.js

--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -2,21 +2,21 @@ import React from "react";
 import "./style.css";
 
 function SearchResults(props) {
-  const { results } = props;
+  const { results, handleFilter, handleSort, sort, toggleSort } = props;
   return (
     <table class="table table-striped table-bordered" id="empTable">
       <thead>
         <tr>
           <th>Yearbook Photo</th>
-          <th>Name</th>
+          <th>Name <button onClick={toggleSort}>{sort ? "DESC" : "ASC"}</button></th>
           <th>Email</th>
           <th>Cell</th>
         </tr>
       </thead>
 
       <tbody>
-        {results.map((result) => (
-          <tr key={result}>
+        {results.sort(handleSort).filter(handleFilter).map((result) => (
+          <tr key={result.email}>
             <td>
               <img src={result.picture.medium} alt={result.name.first}></img>
             </td>
--- src/pages/search.js
+++ src/pages/search.js
@@ -7,54 +7,76 @@ import Header from "../components/Header";
 
 <Header />;
 function Search() {
-  const [searchState,] = useState({
-    search: "",
-    employees: [],
-    results: [],
-    error: "",
-  });
-  const [employee, setEmployee] = useState([]);
+  
   const [results, setResults] = useState([]);
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState(true); // true means "ascending", when false "it is descended"
+
   //hook above!______________________________
   useEffect(() => {
     API.getRandomEmployee()
-      .then((res) => setEmployee(res.data.results))
-      .then((res) => setResults(employee))
+      .then((res) => setResults(res.data.results))
       .catch((err) => console.log(err));
   }, []);
 
   const handleInputChange = (event) => {
     let searchTerm = event.target.value;
-
-    let filtered = employee.filter((query) =>
-      query.name.first.includes(searchTerm)
-    );
-
-    setResults(filtered);
-    console.log(employee);
+    setSearch(searchTerm); // Set search. This will save what user types in search field
   };
 
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-    API.getEmployees(searchState.employees)
-      .then((res) => {
-        if (res.data.status === "error") {
-          throw new Error(res.data.message);
-        }
-        this.setState({ results: res.data.message, error: "" });
-      })
-      .catch((err) => this.setState({ error: err.message }));
-  };
+  // Search by first name.
+  const handleFilter = (query) => {
+      return query.name.first.toLowerCase().includes(search.toLowerCase())
+  }
+
+  // Custom sort example: https://stackoverflow.com/questions/5002848/how-to-define-custom-sort-function-in-javascript
+  const handleSort = (employeeA, employeeB) => {
+    if(sort) { // if sort is true the this means sort is currently ascending. Therefore sort in descending
+      if(employeeA.name.first < employeeB.name.first) {
+        return -1;
+      } else if(employeeA.name.first > employeeB.name.first) {
+        return 1;
+      }
+    } else {
+      if(employeeA.name.first < employeeB.name.first) {
+        return 1;
+      } else if(employeeA.name.first > employeeB.name.first) {
+        return -1;
+      }
+    }
+
+    return 0;
+  }
+
+  const toggleSort = ()=> {
+    setSort(!sort); // toggle sort. This will either swap ASC to DESC OR swap DESC to ASC
+  }
+
+  // const handleFormSubmit = (event) => {
+    // event.preventDefault();
+    // API.getEmployees(searchState.employees)
+    //   .then((res) => {
+    //     if (res.data.status === "error") {
+    //       throw new Error(res.data.message);
+    //     }
+    //     this.setState({ results: res.data.message, error: "" });
+    //   })
+    //   .catch((err) => this.setState({ error: err.message }));
+  // };
 
   return (
     <div>
       <Container style={{ minHeight: "80%" }}>
         <SearchForm
-          handleFormSubmit={handleFormSubmit}
           handleInputChange={handleInputChange}
-          // employees={searchState.employees}
         />
-        <SearchResults results={results} />
+        <SearchResults 
+          results={results} 
+          handleFilter={handleFilter} 
+          handleSort={handleSort} 
+          sort={sort} 
+          toggleSort={toggleSort} 
+        />
       </Container>
     </div>
   );
